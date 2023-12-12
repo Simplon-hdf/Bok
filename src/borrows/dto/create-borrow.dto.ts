34d3ffb221d744283d3a6176584ca5a57970c663
massima <mastem@msn.com>
2023-12-12 14:14:30 +0100

    feat(borrows): config dto and add prisma provider to moduble

--- src/borrows/dto/create-borrow.dto.ts
+++ src/borrows/dto/create-borrow.dto.ts
@@ -1,1 +1,26 @@
-export class CreateBorrowDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { BorrowState } from '@prisma/client';
+import { IsDate, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBorrowDto {
+    @ApiProperty({
+        description: 'this field represents the borrowing date',
+    })
+    @IsDate()
+    started_at: Date;
+
+    @ApiProperty({
+        description:
+            'this field represents the when the book has to be returned',
+    })
+    @IsDate()
+    end_at: Date;
+
+    @ApiProperty({
+        description: 'this field represents the book status',
+    })
+    @IsString()
+    @IsNotEmpty()
+    @IsEnum(BorrowState)
+    status: BorrowState;
+}
