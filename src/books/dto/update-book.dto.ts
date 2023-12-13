d047aa0c9593381a828a1014a1f599696d1a1f01
massima <mastem@msn.com>
2023-12-13 13:48:40 +0100

    feat(books):config book update dto

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateBookDto } from './create-book.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+    @IsOptional()
+    book_name?: string;
+    @IsOptional()
+    description?: string;
+    @IsOptional()
+    authors_UUID?: string;
+    @IsOptional()
+    borrow_UUID?: string;
+}
