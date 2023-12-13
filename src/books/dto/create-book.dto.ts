1c0d37bb4bf7c4dc18483d8dc9cb2535a9642755
massima <mastem@msn.com>
2023-12-13 11:00:23 +0100

    feat(books): add book dto properties

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,26 @@
-export class CreateBookDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateBookDto {
+    @ApiProperty()
+    @IsString()
+    @MaxLength(40)
+    @IsNotEmpty()
+    book_name: string;
+
+    @ApiProperty()
+    @IsString()
+    @MaxLength(500)
+    @IsNotEmpty()
+    description: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    authors_UUID: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    borrow_UUID: string;
+}
